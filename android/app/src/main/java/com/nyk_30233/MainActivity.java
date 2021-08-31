0063fa5249d849dd21e23711c6f416f28d5f0a61
Crowdbotics <team@crowdbotics.com>
2021-08-31 20:06:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nyk_30233/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nyk_30233;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "nyk_30233";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "nyk_30233Identifier",
+  url: "https://nyk_30233Identifier.botics.co",
+  api: "https://nyk_30233Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
